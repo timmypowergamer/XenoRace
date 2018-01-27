0e9fbe333592e17dc599e13653a5b92dd84809db
Alex Madsen <akmadsen522@gmail.com>
2018-01-27 14:07:13 -0700

    Goal triggers now pick up entry

--- XenoRaceUnity/Assets/Scripts/Business Logic/DeathFloor.cs
+++ XenoRaceUnity/Assets/Scripts/Business Logic/DeathFloor.cs
@@ -2,15 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DeathFloor : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+/// <summary>
+/// When the player hits the death floor, we need to restart the game. 
+/// </summary>
+public class DeathFloor : MonoBehaviour
+{
+    /// <summary>
+    /// If the player has entered our trigger, then we need to do our courtesy calls. This will either restart 
+    /// the game immediately, or get the player some kind of "Game Over" card, taking them to the build menu. 
+    /// </summary>
+    /// <param name="other">The collider that has hit us.</param>
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("DEATH FLOOR has been hit by " + other.name); 
+    }
 }
--- XenoRaceUnity/Assets/Scripts/Business Logic/GameManager.cs
+++ XenoRaceUnity/Assets/Scripts/Business Logic/GameManager.cs
@@ -3,18 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// The Game Manager handles the basic functions of the game. 
+/// The Game Manager handles the win and lose conditions of the game. 
 /// </summary>
 public class GameManager : MonoBehaviour
 {
-	// Use this for initialization
-	void Start ()
-    {
-        DontDestroyOnLoad(this);
-	}
+    /// <summary>
+    /// Our singleton instance of the game manager. 
+    /// </summary>
+    private static GameManager instance; 
 
-	// Update is called once per frame
-	void Update ()
+    /// <summary>
+    /// Publically available reference to the game manager instance. 
+    /// </summary>
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance; 
+        }
+    }
+
+    /// <summary>
+    /// Enforce our singleton-itude on awake. If we are the only game manager in the scene, then we *are* the 
+    /// instance. Otherwise, we commit hara-kiri. 
+    /// </summary>
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this; 
+        } else
+        {
+            Destroy(this.gameObject); 
+        }
+    }
+
+    /// <summary>
+    /// Subscribe to any events that we care about on Start.
+    /// </summary>
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update ()
     {
 
 	}
