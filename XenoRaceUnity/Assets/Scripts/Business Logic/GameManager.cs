8395e998b90bb5d9e82590c290459d96e0a86446
Alex Madsen <akmadsen522@gmail.com>
2018-01-27 14:49:42 -0700

    Time is now measured, and we care about it in-game

--- XenoRaceUnity/Assets/Scripts/Business Logic/GameManager.cs
+++ XenoRaceUnity/Assets/Scripts/Business Logic/GameManager.cs
@@ -23,6 +23,47 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    #region GameManager CoreVariables 
+
+    /// <summary>
+    /// How many seconds we start with for our track. (Should live in another data class?)
+    /// </summary>
+    [SerializeField] private int startingSeconds = 60;
+
+    /// <summary>
+    /// How much time is left in seconds. 
+    /// </summary>
+    private float timeLeft;
+
+    /// <summary>
+    /// Measures if the game is over. 
+    /// </summary>
+    private bool isGameOver = false; 
+
+    #endregion 
+
+    /// <summary>
+    /// The remaining time in seconds. 
+    /// </summary>
+    public float TimeLeft
+    {
+        get
+        {
+            return timeLeft; 
+        }
+    }
+
+    /// <summary>
+    /// Returns true if the game is over, false otherwise. 
+    /// </summary>
+    public bool IsGameOver
+    {
+        get
+        {
+            return isGameOver; 
+        }
+    }
+
     /// <summary>
     /// Enforce our singleton-itude on awake. If we are the only game manager in the scene, then we *are* the 
     /// instance. Otherwise, we commit hara-kiri. 
@@ -31,7 +72,8 @@ public class GameManager : MonoBehaviour
     {
         if(instance == null)
         {
-            instance = this; 
+            instance = this;
+            DontDestroyOnLoad(this); 
         } else
         {
             Destroy(this.gameObject); 
@@ -39,31 +81,76 @@ public class GameManager : MonoBehaviour
     }
 
     /// <summary>
-    /// Subscribe to any events that we care about on Start.
+    /// Set up our variables when we start the race. 
     /// </summary>
     private void Start()
     {
-        
+        InitializeRace(); 
     }
 
-    // Update is called once per frame
-    void Update ()
+    /// <summary>
+    /// Track any game metrics we are about. 
+    /// </summary>
+    private void Update()
     {
-
-	}
-
-    void HandleRace()
-    {
-
+        if(timeLeft > 0 && !isGameOver)
+        {
+            DecrementTimer();
+        }
+   
     }
 
-    public void LoadNewTrack(string trackName)
+    /// <summary>
+    /// Decrement our timer. If we hit zero, then we need to call whoever cares. 
+    /// </summary>
+    private void DecrementTimer()
     {
+        timeLeft -= Time.deltaTime;
+        timeLeft = timeLeft < 0 ? 0 : timeLeft; // No negative times. 
 
+        if(timeLeft == 0)
+        {
+            TimedOut(); 
+        }
     }
 
-    public void RestartTrack()
+    /// <summary>
+    /// Function to call if the player has run out of time. 
+    /// </summary>
+    private void TimedOut()
     {
+        isGameOver = true; 
+        Debug.Log("PLAYER HAS TIMED OUT"); 
+    }
 
+    /// <summary>
+    /// Set the variables that we need for the race to start okay. 
+    /// </summary>
+    private void InitializeRace()
+    {
+        isGameOver = false;
+        timeLeft = startingSeconds; 
+    }
+
+    /// <summary>
+    /// Function to call if the player has fallen off the track. 
+    /// </summary>
+    public void FellOffTrack()
+    {
+        Debug.Log("PLAYER FELL OFF THE TRACK"); 
+    }
+
+    public void PlayerEnteredGoal()
+    {
+        Debug.Log("PLAYER ENTERED THE GOAL TRIGGER"); 
+    }
+
+    /// <summary>
+    /// Destroys the game manager - used for when we leave the actual "Game"
+    /// </summary>
+    public void Kill()
+    {
+        GameManager.instance = null;
+        Destroy(this); 
     }
 }
--- XenoRaceUnity/Assets/Scripts/UI/Game UI Utilities/TimerDisplay.cs
+++ XenoRaceUnity/Assets/Scripts/UI/Game UI Utilities/TimerDisplay.cs
@@ -1,16 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
+/// <summary>
+/// Only concerned with the number of seconds left, currently down to the millisecond. 
+/// </summary>
 public class TimerDisplay : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+    /// <summary>
+    /// The field of text we want to display in. 
+    /// </summary>
+    [SerializeField]
+    private Text displayText;
+
+    // Our time measurement scratchpad variables. 
+    private int minutes = 0;
+    private int seconds = 0;
+    private int milliseconds = 0; 
+
+
+	/// <summary>
+    /// Update the displayed time every frame 
+    /// </summary>
+	private void Update ()
+    {
+        displayText.text = GameManager.Instance.TimeLeft.ToString(); 
 	}
 }
