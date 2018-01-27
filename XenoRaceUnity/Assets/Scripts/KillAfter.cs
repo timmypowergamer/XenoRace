254259f84a3029a20366d30d85988187d8489fdf
Ryan Dallaire <tastyhippo@gmail.com>
2018-01-27 14:08:53 -0700

    Blob splat spawner

---
+++ XenoRaceUnity/Assets/Scripts/KillAfter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RyanDStandard
+{
+    namespace Events
+    {
+        public class KillAfter : MonoBehaviour
+        {
+            public float Delay = 1f;
+            private float spawnTime = 0f;
+
+            // Use this for initialization
+            void Start()
+            {
+                spawnTime = Time.time;
+            }
+
+            // Update is called once per frame
+            void Update()
+            {
+                if (Time.time > spawnTime + Delay)
+                {
+                    GameObject.Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
---
+++ XenoRaceUnity/Assets/Scripts/SplatSpawn.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplatSpawn : MonoBehaviour
+{
+    public GameObject splatPrefab;
+    private ParticleSystem part;
+    private List<ParticleCollisionEvent> events = new List<ParticleCollisionEvent>();
+
+    // Use this for initialization
+    void Start()
+    {
+        part = GetComponent<ParticleSystem>();
+    }
+    void OnParticleCollision(GameObject other)
+    {
+        int totalCollisions = part.GetCollisionEvents(other, events);
+
+        foreach (ParticleCollisionEvent thing in events)
+        {
+            GameObject obj = Instantiate(splatPrefab, thing.intersection, Quaternion.Euler(thing.normal));
+            //Debug.Log("col= " + thing.intersection);
+            //Debug.Break();
+            obj.transform.Rotate(0f, Random.Range(0f, 240f), 0f);
+        }
+    }
+}
