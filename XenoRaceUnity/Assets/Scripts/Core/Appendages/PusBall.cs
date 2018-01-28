ba5de04bb18ee50a9b4f93b1fa833771a85d5553
Ryan Dallaire <tastyhippo@gmail.com>
2018-01-27 21:37:29 -0700

    pusBall started

---
+++ XenoRaceUnity/Assets/Scripts/Core/Appendages/PusBall.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PusBall : Appendage {
+    public float force = 1f;
+    public float radius = 0.25f;
+
+	void FinishedBlowingUp()
+    {
+        _coreRB.AddExplosionForce(force, transform.position, radius);
+    }
+}
