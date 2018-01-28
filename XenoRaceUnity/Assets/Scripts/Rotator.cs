4efdd7bc7ca92e621e9300764be3170c85bbf3c2
Ryan Dallaire <tastyhippo@gmail.com>
2018-01-28 15:08:58 -0700

    Menu logo and rotator

---
+++ XenoRaceUnity/Assets/Scripts/Rotator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+    [SerializeField]
+    public enum Axes
+    {
+        x,
+        y,
+        z
+    }
+    public Axes spinDirection = Axes.y;
+    public float Speed;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		switch (spinDirection)
+        {
+            case Axes.x:
+                transform.Rotate(Speed * Time.deltaTime,0f, 0f);
+                break;
+            case Axes.y:
+                transform.Rotate(0f, Speed * Time.deltaTime, 0f);
+                break;
+            case Axes.z:
+                transform.Rotate(0f, 0f, Speed * Time.deltaTime);
+                break;
+        }
+	}
+}
