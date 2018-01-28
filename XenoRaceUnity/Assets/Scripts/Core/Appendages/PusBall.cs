f67eb81e524eba7f8a79127ef00d70f055c99c39
Ryan Dallaire <tastyhippo@gmail.com>
2018-01-28 10:16:22 -0700

    pus splat and sound. leg sound.

--- XenoRaceUnity/Assets/Scripts/Core/Appendages/PusBall.cs
+++ XenoRaceUnity/Assets/Scripts/Core/Appendages/PusBall.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PusBall : Appendage {
     public float force = 1f;
     public float radius = 0.25f;
+    public ParticleSystem particles;
 
 	public void FinishedBlowingUp()
     {
         _coreRB.AddExplosionForce(force, transform.position, radius, 1f, ForceMode.Impulse);
+        particles.Emit(50);
     }
 }
