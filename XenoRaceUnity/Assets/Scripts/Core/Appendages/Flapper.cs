8acaf275d3fbee5b52b82d2f8c636c866f02d84d
Ryan Dallaire <tastyhippo@gmail.com>
2018-01-28 00:20:00 -0700

    spiker part

--- XenoRaceUnity/Assets/Scripts/Core/Appendages/Flapper.cs
+++ XenoRaceUnity/Assets/Scripts/Core/Appendages/Flapper.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Flapper : Appendage {
+    public float ImpulseSpeed = 0f;
 
-
+    public override void OnActivateStart()
+    {
+        base.OnActivateStart();
+        _coreRB.AddForce(-transform.up * ImpulseSpeed);
+    }
 }
