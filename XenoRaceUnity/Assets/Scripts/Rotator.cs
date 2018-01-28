f07f165418156e44bd62eb7a9e436295047581d2
Ryan Dallaire <tastyhippo@gmail.com>
2018-01-28 15:38:41 -0700

    Main menu fixes

--- XenoRaceUnity/Assets/Scripts/Rotator.cs
+++ XenoRaceUnity/Assets/Scripts/Rotator.cs
@@ -13,6 +13,10 @@ public class Rotator : MonoBehaviour
     }
     public Axes spinDirection = Axes.y;
     public float Speed;
+    public float HoverSpeed;
+    public float HoverTimeout;
+    private float TimeSinceChange = 0f;
+    private bool GoingUp = false;
 	
 	// Update is called once per frame
 	void Update ()
@@ -29,5 +33,15 @@ public class Rotator : MonoBehaviour
                 transform.Rotate(0f, 0f, Speed * Time.deltaTime);
                 break;
         }
-	}
+        TimeSinceChange += Time.deltaTime;
+        if (GoingUp)
+            transform.position += new Vector3(0f, HoverSpeed * Time.deltaTime, 0f);
+        else
+            transform.position -= new Vector3(0f, HoverSpeed * Time.deltaTime, 0f);
+        if (TimeSinceChange > HoverTimeout)
+        {
+            GoingUp = !GoingUp;
+            TimeSinceChange = 0f;
+        }
+    }
 }
