a8fd7fc601a74410afd3e226531250296d03c3b3
Ryan Dallaire <tastyhippo@gmail.com>
2018-01-27 19:24:43 -0700

    Track texture with animation

---
+++ XenoRaceUnity/Assets/Scripts/EmisAnimate.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmisAnimate : MonoBehaviour
+{
+    [Header("!!Use Standard Shader (No sprites)!!")]
+
+    [Header("Material1")]
+    public bool AnimateEmis = false;
+    public float Speed = 0f;
+    private Color TargetColor;
+    public bool goingUp = false;
+
+    private Material rendererMat1;
+
+    // Use this for initialization
+    void Start()
+    {
+        Renderer rend = GetComponent<Renderer>();
+        if (rend != null)//if it found a renderer
+        {
+            rendererMat1 = rend.material;//get material 0 from renderer
+            Material[] mats = rend.materials;//gets array of all materials
+            TargetColor = rendererMat1.GetColor("_EmissionColor");
+        }
+        else
+            Debug.LogError("EmisAnimate: NO RENDERER ON " + gameObject.name, this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (AnimateEmis)
+        {
+            if (rendererMat1 != null)
+            {
+                Color emis = rendererMat1.GetColor("_EmissionColor");
+                if (goingUp)
+                {
+                    if (emis.r < TargetColor.r)
+                    {
+                        emis.r += Speed * Time.deltaTime;
+                        if (emis.r > TargetColor.r)
+                            emis.r = TargetColor.r;
+                    }
+                    if (emis.g < TargetColor.g)
+                    {
+                        emis.g += Speed * Time.deltaTime;
+                        if (emis.g > TargetColor.g)
+                            emis.g = TargetColor.g;
+                    }
+                    if (emis.b < TargetColor.b)
+                    {
+                        emis.b += Speed * Time.deltaTime;
+                        if (emis.b > TargetColor.b)
+                            emis.b = TargetColor.b;
+                    }
+                }
+                else
+                {
+                    if (emis.r > 0f)
+                    {
+                        emis.r -= Speed * Time.deltaTime;
+                        if (emis.r < 0f)
+                            emis.r = 0f;
+                    }
+
+                    if (emis.g > 0f)
+                    {
+                        emis.g -= Speed * Time.deltaTime;
+                        if (emis.g < 0f)
+                            emis.g = 0f;
+                    }
+
+                    if (emis.b > 0f)
+                    {
+                        emis.b -= Speed * Time.deltaTime;
+                        if (emis.b < 0f)
+                            emis.b = 0f;
+                    }
+                }
+                if (goingUp && emis.r == TargetColor.r && emis.g == TargetColor.g && emis.b == TargetColor.b)
+                    goingUp = !goingUp;
+                if (!goingUp && emis.r == 0f && emis.g == 0f && emis.b == 0f)
+                    goingUp = !goingUp;
+                rendererMat1.SetColor("_EmissionColor", emis);
+                GetComponent<Renderer>().UpdateGIMaterials();
+            }
+            else
+                Debug.LogError("EmisAnimate: Renderer Material 1 missing on " + gameObject.name, this);
+        }
+    }
+}
---
+++ XenoRaceUnity/Assets/Scripts/UVAnimate.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RyanDStandard
+{
+    public class UVAnimate : MonoBehaviour
+    {
+        [Header("!!Use Standard Shader (No sprites)!!")]
+
+        [Header("Material1")]
+        public bool AnimateUV1 = false;
+        public float U1Speed = 0f;
+        public float V1Speed = 0f;
+        [Space(15f)]
+        [Space(0f)]
+        [Header("Material2")]
+        public bool AnimateUV2 = false;
+        public float U2Speed = 0f;
+        public float V2Speed = 0f;
+
+        private Material rendererMat1;
+        private Material rendererMat2;
+
+        // Use this for initialization
+        void Start()
+        {
+            Renderer rend = GetComponent<Renderer>();
+            if (rend != null)//if it found a renderer
+            {
+                rendererMat1 = rend.material;//get material 0 from renderer
+                Material[] mats = rend.materials;//gets array of all materials
+                if (mats.Length > 1)//if there are more than one material
+                {
+                    rendererMat2 = mats[1];//find the second material
+                }
+            }
+            else
+                Debug.LogError("UVAnimate: NO RENDERER ON " + gameObject.name, this);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (AnimateUV1)
+            {
+                if (rendererMat1 != null)
+                {
+                    Vector2 newOffset = rendererMat1.GetTextureOffset("_MainTex");
+                    newOffset += new Vector2(U1Speed, V1Speed) * Time.deltaTime;
+                    rendererMat1.SetTextureOffset("_MainTex", newOffset);
+                }
+                else
+                    Debug.LogError("UVAnimate: Renderer Material 1 missing on " + gameObject.name, this);
+            }
+
+            if (AnimateUV2)
+            {
+                if (rendererMat2 != null)
+                {
+                    Vector2 newOffset = new Vector2(U2Speed, V2Speed) * Time.deltaTime;
+                    rendererMat2.SetTextureOffset("_MainTex", newOffset);
+                }
+                else
+                    Debug.LogError("UVAnimate: Renderer Material 2 missing on " + gameObject.name, this);
+            }
+        }
+    }
+}
